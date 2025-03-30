40bf242aef3ccd465d93c00595409e97c4df7a6f
WillianRD <willianrodriguesdos1@gmail.com>
2025-03-29 21:14:57 -0300

    Style

--- app.py
+++ app.py
@@ -5,12 +5,21 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        name: str = request.form['name']
-        autor: str = request.form['autor']
-        temporadas: int = request.form['temporadas']
-        imagem: str = request.form['img']
-
-        print(f"Nome:{name}\nAutor:{autor}\nTemporadas:{temporadas}\nimagem:{imagem}")
+        produto: str = request.form['produto']
+        categoria: str = request.form['categoria']
+        descricao: str = request.form['descricao']
+        preco: str = request.form['preco']
+        quantidadeEstoque: int = request.form['estoque']
+        fornecedor: str = request.form['fornecedor']
+        fabricacao: str = request.form['fabricacao']
+        vencimento: str = request.form['vencimento']
+        caracteristicas: str = request.form['caracteristicas']
+        url: str = request.form['url']
+        
+        print(f'Produto:{produto}')
+        print(f'Categoria{categoria}Descrição{descricao}')
+        print(f'Preço{preco}Quantidade Estoque{quantidadeEstoque}Fornecedor{fornecedor}')
+        print(f'Fabricação{fabricacao}Vencimento{vencimento}Caracteristicas{caracteristicas}URL:{url}')
     return render_template('index.html')
 
 
