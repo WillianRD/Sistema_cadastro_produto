17505fc37685473c8cec644ff72bb2ad3eb9bf1b
WillianRD <willianrodriguesdos1@gmail.com>
2025-03-29 22:22:57 -0300

    Validar Categoria

---
+++ validarCategoria.py
@@ -0,0 +1,20 @@
+def checkCategoria(categoria):
+    if categoria == 0:
+        return False
+    
+    if categoria == 'Alimentos':
+        return True
+    
+    if categoria == 'Bebidas':
+        return True
+    
+    if categoria == 'Roupas':
+        return True
+    
+    if categoria == 'Tecnologia':
+        return True
+    
+    if categoria == 'Saúde':
+        return True
+    
+    return False
