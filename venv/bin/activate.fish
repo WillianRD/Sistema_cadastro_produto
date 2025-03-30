c74889f57fa2d815079c4ef93ee5657342087981
WillianRD <willianrodriguesdos1@gmail.com>
2025-03-30 09:29:52 -0300

    Reload Project

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/will/Documentos/Python/Dona Leide/venv'
+set -gx VIRTUAL_ENV /home/will/dev-experiments-Flask/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
