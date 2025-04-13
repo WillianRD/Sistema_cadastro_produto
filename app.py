07a786fc6395c4dec0b326594296cd88a9399446
WillianRD <willianrodriguesdos1@gmail.com>
2025-04-13 20:30:01 -0300

    Nova atualização sobre o projeto
    Atualizado as imagens que não carregava
    
    Organização das pastas do banco, agora ficam na pasta Controller
    
    Comentário sobre as linhas de comandos

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template, request, url_for # type: ignore
-from validar_produto.validarProduto import checkSize,checkCategory
-from validar_produto.validarProduto import checkPrice, checkDesc, checkSizeEstoque,checkFornecedor
-from validar_produto.validarProduto import caracteres, url
-from models import updateDados, ReadDados
+from flask import Flask, render_template, request
+from Controller import connect_data, read_data, update_data, delete
+
+
+# Chama a instancia do Flask
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def main():
-    return 'Página principal --- Em produção'
-
-@app.route("/produtos/insert", methods=['POST','GET'])
+# Rota criada
+@app.route("/", methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        produto = request.form['produto']
+        produto = request.form['produto'] # Variavel produto recebe requisição do form do atributo produto do index.html
         categoria = request.form['categoria']
         preco = float( request.form['preco'])
         descricao = request.form['descricao']
@@ -21,22 +18,14 @@ def index():
         fabricacao= request.form['fabricacao']
         vencimento= request.form['vencimento']
         caracteristicas= request.form['caracteristicas']
-        link_image = request.form['page_url']
-        updateDados(produto,categoria,descricao,preco,qtd,fabricacao,vencimento,caracteristicas,link_image)
+        url_link_page = request.form['urllink']
+        update(produto,categoria,descricao,preco,qtd,fabricacao,vencimento,caracteristicas,url_link_page)
     return render_template('index.html')
 
-@app.route("/produtos/read", methods=['GET', 'POST'])
+@app.route("/produtos/read", methods=['GET'])
 def get_produto():
     listaDeProdutos = ReadDados()
     return render_template('form_add.html',lista=listaDeProdutos)
-
-@app.route("/produtos/delete", methods=['GET','DELETE'])
-def delete_produto():
-    return '<h1>Essa rota vai servir para deletar dados --- Em produção </h1>'
-    
-@app.route('/produtos/update', methods=['GET']) 
-def update():
-    return 'Atualizar dados --- Em produção'   
 
 if __name__ == '__main__':
     app.run(debug=True)
